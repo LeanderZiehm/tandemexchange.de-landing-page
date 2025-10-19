7ba3690ddbbca5cca80dc811cc38b3f1d955ba84
Leander-Arun Ziehm <117998178+LeanderZiehm@users.noreply.github.com>
2025-10-19 14:20:56 +0200

    Integrate Matomo analytics in layout component
    Added Matomo analytics tracking script to the layout.

--- app/layout.jsx
+++ app/layout.jsx
@@ -1,6 +1,6 @@
 import { DM_Sans } from "next/font/google";
 import "./globals.css";
-import Script from "next/script"; // ✅ import Script
+import Script from "next/script";
 
 const dmSans = DM_Sans({
   subsets: ["latin"],
@@ -24,6 +24,35 @@ export default function RootLayout({ children }) {
           src="https://umami.leanderziehm.com/script.js"
           data-website-id="99fcf915-b73e-42d4-b469-0e1ddab35e03"
         />
+
+        {/* Matomo analytics */}
+        <Script id="matomo-init" strategy="afterInteractive">
+          {`
+            var _paq = window._paq = window._paq || [];
+            _paq.push(["setDocumentTitle", document.domain + "/" + document.title]);
+            _paq.push(['trackPageView']);
+            _paq.push(['enableLinkTracking']);
+            (function() {
+              var u="//matomo.leanderziehm.com/";
+              _paq.push(['setTrackerUrl', u+'matomo.php']);
+              _paq.push(['setSiteId', '5']);
+              var d=document, g=d.createElement('script'), s=d.getElementsByTagName('script')[0];
+              g.async=true; g.src=u+'matomo.js'; s.parentNode.insertBefore(g,s);
+            })();
+          `}
+        </Script>
+
+        <noscript>
+          <p>
+            <img
+              referrerPolicy="no-referrer-when-downgrade"
+              src="//matomo.leanderziehm.com/matomo.php?idsite=5&amp;rec=1"
+              style={{ border: 0 }}
+              alt=""
+            />
+          </p>
+        </noscript>
+
         {children}
       </body>
     </html>
